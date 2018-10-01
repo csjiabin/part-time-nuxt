f02403e2f4e5a85aa4fd865e18b665448e8b2473
csjiabin <1187712426@qq.com>
2018-10-01 10:25:31 +0800

    init dev

--- pages/art.vue
+++ pages/art.vue
@@ -3,21 +3,13 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        part-time-vue
+        part-time-nuxt
       </h1>
       <h2 class="subtitle">
         Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-          <nuxt-link to="/art">关于</nuxt-link>
+          <nuxt-link to="/users/1">users/1</nuxt-link>
       </div>
     </div>
   </section>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        part-time-vue
+        part-time-nuxt
       </h1>
       <h2 class="subtitle">
         Nuxt.js project
